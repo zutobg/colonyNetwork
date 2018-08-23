9545a47d1895b4fd2c20a43c9c771b84dc55a25e
elenadimitrova <elenavdimitrova@gmail.com>
2018-08-23 14:01:51 +0300

    Fix an issue with updating colony wide reputation for child skills

--- packages/reputation-miner/ReputationMiner.js
+++ packages/reputation-miner/ReputationMiner.js
@@ -221,6 +221,7 @@ class ReputationMiner {
       const logEntryNumber = await this.getLogEntryNumberForLogUpdateNumber(logEntryUpdateNumber, blockNumber);
       logEntry = await repCycle.getReputationUpdateLogEntry(logEntryNumber, { blockNumber });
       score = this.getScore(updateNumber, logEntry[1]);
+
       // When reputation score update is negative, adjust its value for child reputation updates
       if (score.lt(0)) {
         // For reputation loss, when updating child skills, adjust reputation amount lost
@@ -244,7 +245,6 @@ class ReputationMiner {
             .sub(innerUpdateNumber)
             .add(nUpdates)
             .sub(1);
-
           const keyForActualUserSkillRep = await this.getKeyForUpdateNumber(updateNumberForActualUserSkillRep.toNumber());
 
           const keyAlreadyExists = this.reputations[keyForActualUserSkillRep] !== undefined;
@@ -254,12 +254,18 @@ class ReputationMiner {
             const existingValueForActualUserSkillRep = ethers.utils.bigNumberify(`0x${valueForActualUserSkillRep.slice(2, 66)}`);
 
             if (!existingValueForActualUserSkillRep.isZero()) {
-              const key = await this.getKeyForUpdateNumber(updateNumber);
+              let key;
+              // For colony wide reputation updates, consider the key of the actual user reputation
+              if (innerUpdateNumber.lt(nChildUpdates)) {
+                const updateNumberForActualChildSkillRep = updateNumber.add(nUpdates.div(2));
+                key = await this.getKeyForUpdateNumber(updateNumberForActualChildSkillRep);
+              } else {
+                key = await this.getKeyForUpdateNumber(updateNumber);
+              }
 
               const keyExists = this.reputations[key] !== undefined;
               if (keyExists) {
                 const reputation = ethers.utils.bigNumberify(`0x${this.reputations[key].slice(2, 66)}`);
-
                 // todo bn.js doesn't have decimals so is fraction precision enough here?
                 const targetScore = reputation.mul(score).div(existingValueForActualUserSkillRep);
                 score = targetScore;
